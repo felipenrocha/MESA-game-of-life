d9f559471d9653ee6de7e1a8d249acc0864a8a5f
felipenrocha19 <felipenrocha19@gmail.com>
2022-07-21 12:25:09 -0300

    finished money model- starting game of life

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from money_model import MoneyModel
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def main(): 
-    modelo = MoneyModel(10)
+    modelo = MoneyModel(50, 10, 10)
     # loop para dar 10 passos:
-    for i in range(10):
+    for i in range(20):
         modelo.step() 
+        
+    agent_counts = np.zeros((modelo.grid.width, modelo.grid.height))
+    for cell in modelo.grid.coord_iter():
+        cell_content, x, y = cell
+        agent_count = len(cell_content)
+        agent_counts[x][y] = agent_count
+    gini = modelo.datacollector.get_model_vars_dataframe()
+    gini.plot()
+    plt.imshow(agent_counts, interpolation="nearest")
+    plt.colorbar()
+    plt.show()
+
+
     
     
     
@@ -18,18 +33,18 @@ def main():
 
 
     # media de distribuicao depois de 100 steps em que cada step gera 10 agentes
-    toda_riqueza = []
-    for j in range(100):
-        model = MoneyModel(10)
-        for i in range(10):
-            model.step()
+    # toda_riqueza = []
+    # for j in range(100):
+    #     model = MoneyModel(10)
+    #     for i in range(10):
+    #         model.step()
 
-        # Store the results
-        for agent in model.schedule.agents:
-           toda_riqueza.append(agent.riqueza)
+    #     # Store the results
+    #     for agent in model.schedule.agents:
+    #        toda_riqueza.append(agent.riqueza)
 
-    plt.hist(toda_riqueza, bins=range(max(toda_riqueza) + 1))
-    plt.show()
+    # plt.hist(toda_riqueza, bins=range(max(toda_riqueza) + 1))
+    # plt.show()
 
 
 
